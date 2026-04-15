e52c5ca74abe421a677c486b8a8c1c9d8f3ccfdd
Valentin Lucet <valentin.lucet@gmail.com>
2026-04-14 22:00:45 -0400

    add melanie's code to a function

--- functions.R
+++ functions.R
@@ -48,6 +48,50 @@ my_mean <- function(x) {
   mean(x)
 }
 
+process_hist_data <- function(t_hist) {
+  # Code contributed by M. Brochu
+  # 3. Reshape from wide to long
+  t_hist_long <- t_hist %>%
+    pivot_longer(
+      cols      = -Region,
+      names_to  = "Metric",
+      values_to = "Value"
+    )
+
+  # 4. Recode metric labels
+  t_hist_long$Metric <- recode(
+    t_hist_long$Metric,
+    "X2020.2024.Increase" = "2020–2024 Increase",   
+    "X2020.Baseline"      = "2020 Baseline"
+  )
+
+  # 5. Rename & reorder regions
+  t_hist_long$Region <- recode(
+    t_hist_long$Region,
+    "Terrestrial total" = "Terrestrial",
+    "Marine total"      = "Marine"
+  )
+
+  t_hist_long$Region <- factor(
+    t_hist_long$Region,
+    levels = c(
+      sort(setdiff(unique(t_hist_long$Region), c("Terrestrial", "Marine"))),
+      "Terrestrial",
+      "Marine"
+    )
+  )
+
+  # 6. Create grouping variable for totals panel
+  t_hist_long$Group <- ifelse(
+    t_hist_long$Region %in% c("Terrestrial", "Marine"),
+    "Totals",
+    "Province/Territory"
+  )
+
+  return(t_hist_long)
+
+}
+
 # compute_centroids <- function(xy_mat, values_mat) {
 #   xy_mat_scaled <- scale(xy_mat) |> as.matrix()
 #   values_mat_norm <- sweep(values_mat, 2, colSums(values_mat), "/") |> as.matrix()
